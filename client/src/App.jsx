6100e7ecb392761ccbfa84096dcbe0e67386962a
Farida Hesham <148005519+Fh-19@users.noreply.github.com>
2025-05-01 01:48:20 +0300

    fixed SignUp and package files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,9 @@ import SigninForm from "./pages/Signin/Signin";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignUp from "./pages/Signup/Signup"; // Make sure to import your SignUp component
 import SignIn from "./pages/Signin/Signin";
-<<<<<<< HEAD
 import Profile from "./pages/Profile/Profile";
 
-
-=======
-import Profile from "./pages/Profile"
 import Homepage from "./pages/HomePage/Home";
->>>>>>> c5806424d51067f26364f78d1f1cf6b59910c0a7
 
 function App() {
   return (
