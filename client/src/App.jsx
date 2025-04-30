f8d808ae8d41985571644bc24b6cb5ffa39a837c
nourFattah <nour.fattah03@eng-st.cu.edu.eg>
2025-04-30 23:06:41 +0300

    accepted all changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignupForm from "./pages/Signup/Signup";
-// import SignupForm from './pages/Signup/Try';
-// import Signin from './components/signin';
-import SigninForm from "./pages/Signin/Signin";
+import SignUp from "./pages/Signup/Signup";
+import SignIn from "./pages/Signin/Signin";
+import Profile from "./pages/Profile";
+import Homepage from "./pages/HomePage/Home";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import SignUp from "./pages/Signup/Signup"; // Make sure to import your SignUp component
-import SignIn from "./pages/Signin/Signin";
-<<<<<<< HEAD
-import Profile from "./pages/Profile/Profile";
-
-
-=======
-import Profile from "./pages/Profile"
-import Homepage from "./pages/HomePage/Home";
->>>>>>> c5806424d51067f26364f78d1f1cf6b59910c0a7
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} /> //trying to make the default the homepage
+        <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/profile" element={<Profile />} />
-        
-        {/* Add other routes as necessary */}
       </Routes>
-      {/* <Signup/> */}
     </Router>
-    // <SigninForm/>
   );
 }
+
 export default App;
