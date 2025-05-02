ec809b5f844f46c85c2c90120bbe5b0d5af88fa5
nourFattah <nour.fattah03@eng-st.cu.edu.eg>
2025-05-02 18:45:17 +0300

    small modification

--- client/src/pages/HomePage/Header.jsx
+++ client/src/pages/HomePage/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/AboutUs">About Us</a></li>
+          <li><a href="#">About Us</a></li>
           <li
             className="dropdown"
             onMouseEnter={toggleDropdown}
